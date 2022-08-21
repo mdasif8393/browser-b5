c9a1c678e98d82bf3a0374bef5e0deec7d568b4f
mdasif8393 <md.asif.8393@gmail.com>
2022-08-21 16:08:26 +0600

    add 2 files

---
+++ timeout.js
@@ -0,0 +1,13 @@
+function doSomething() {
+    console.log('I am coding javascript');
+}
+
+console.log('a')
+console.log('b')
+console.log('c')
+// setTimeout(doSomething, 2000);
+setTimeout(() => {
+    console.log("do dodo")
+},3000);
+console.log('d')
+console.log('e')
